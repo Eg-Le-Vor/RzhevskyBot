ce72e05d45d7a9cf96d5a295d90febf3342ef692
unknown <at-for-job@yandex.ru>
2023-05-01 10:23:23 +0300

    Joke parser changed

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher, types
+from aiogram.utils.markdown import link
 from aiogram.utils import executor
 
-from joke_parser import Joke
+from joke_parser import get_joke
 
 from config import TOKEN
 
@@ -15,9 +16,8 @@ dispatcher = Dispatcher(bot)
 @dispatcher.message_handler(commands=['start'])
 async def start(message: types.Message):
     print(f'ИД чата: {message.chat.id}')
-    joke = Joke()
-    joke.get_joke()
-    await message.answer(f'*Внимание, анекдот!*\n\n{joke.get_joke()}', parse_mode='Markdown')
+    joke_number, joke = await get_joke(list(range(1, 1143)))
+    await message.answer(f'*Внимание, анекдот!*\n\n{joke}', parse_mode='Markdown')
 
 
 if __name__ == '__main__':
--- joke_parser.py
+++ joke_parser.py
@@ -5,18 +5,17 @@ import random
 
 
 BASE_URL = 'https://baneks.ru/'
-JOKE_COUNT = 1143
+# JOKE_COUNT = 1143
 
 
-class Joke:
+# class Joke:
 
-    def __init__(self):
-        self.numbers = list(range(1, JOKE_COUNT))
+#     def __init__(self, available_jokes):
+#         self.numbers = list(range(1, available_jokes))
 
-    def get_joke(self):
-        random_number = random.SystemRandom().choice(self.numbers)
-        request = requests.get(BASE_URL + str(random_number))
-        request.encoding = 'utf-8'
-        joke = BeautifulSoup(request.text, 'lxml').find('p').text.replace('\n\n', '\n').strip().strip('\n')
-        self.numbers.remove(random_number)
-        return joke
+async def get_joke(available_jokes: list):
+    joke_number = random.SystemRandom().choice(available_jokes)
+    joke_request = requests.get(BASE_URL + str(joke_number))
+    joke_request.encoding = 'utf-8'
+    joke = BeautifulSoup(joke_request.text, 'lxml').find('p').text.replace('\n\n', '\n').strip().strip('\n')
+    return joke_number, joke
